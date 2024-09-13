ac34c2fd82953897101d8b4abd20eace5cd6ba24
Duy Việt Võ <duyviet1810@gmail.com>
2024-09-13 11:35:34 +0700

    chore; update dockerfile

--- financial_management/settings.py
+++ financial_management/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = "financial_management.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "drf_grapql",
-        "USER": "postgres",
-        "PASSWORD": "vdv1810",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
